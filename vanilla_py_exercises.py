c87ea7ca9a35d2390aeb0e15e73ba3a5379d22df
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-03-31 18:28:29 +0200

    first commit

---
+++ vanilla_py_exercises.py
@@ -0,0 +1,35 @@
+# arrayOfNums = [1,2,3,47,11,55,4]
+# sortedNumbers = []
+
+# for num in arrayOfNums: 
+#     if num > 30: 
+#         sortedNumbers.append(num)
+
+
+# print(sortedNumbers)
+
+# cityName = input('What is your Name ?')
+
+# animalName = input('What is your Dog Name ?')
+
+# bandName = cityName + " " + animalName
+
+# print(bandName)
+
+# string = 'paperino  '
+
+# print(len(string))
+
+# print(len(string))
+
+# stringB = '  pap e rino  '
+# strippedStringB = stringB.strip()
+
+# print(strippedStringB)
+
+# print(len(stringB.strip()))
+
+name = "Lorenzo"    
+name += "Gigi Riva"
+
+print(name)
